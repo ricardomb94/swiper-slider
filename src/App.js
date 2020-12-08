dc224d5c776fd02d4ed95ecab3cc9799706aaa10
ricardomb94 <aymardmb@gmail.com>
2020-12-08 17:29:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
+import swiper from 'swiper/swiper-bundle.css';
+import './styles.css';
+
+SwiperCore.use([Navigation, Pagination]);
 
 function App() {
+  const slides = [];
+
+  for (let i = 0; i < 5; i += 1) {
+    slides.push(
+      <SwiperSlide key={`slider-${i}`} tag="li">
+        <img
+          src={`https://picsum.photos/id/${i + 1}/500/300`}
+          style={{ listStyle: 'none' }}
+          alt={`Slide ${1}`}
+        />
+      </SwiperSlide>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Swiper
+        id="main"
+        tag="section"
+        wrapperTag="ul"
+        navigation
+        pagination
+        spaceBetween={0}
+        slidesperview={1}
+        onInit={(swiper) => console.log('Swiper initialized', swiper)}
+        onSlideChange={(swiper) =>
+          console.log('Slide index Swiper on change :', activeIndex)
+        }
+        onReachEnd={(swiper) => console.log('Swiper and onReachEnd')}>
+        {slides}
+      </Swiper>
+    </>
   );
 }
 
